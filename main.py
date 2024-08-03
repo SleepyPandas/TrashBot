ca6dccc8345d5ed8fc10677475f69f267c0c597c
PoloJeans <dylanknguyen04@gmail.com>
2024-08-03 17:07:46 -0400

    Update main.py
    First commit for gemini api

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+import google.generativeai as genai
 
+genai.configure(api_key=os.environ['API_KEY'])
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Upload the file and print a confirmation.
+sample_file = genai.upload_file(path="jetpack.jpg",
+                            display_name="Jetpack drawing")
 
+print(f"Uploaded file '{sample_file.display_name}' as: {sample_file.uri}")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    print_hi('AAAAAA')
+file = genai.get_file(name=sample_file.name)
+print(f"Retrieved file '{file.display_name}' as: {sample_file.uri}")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Choose a Gemini model.
+model = genai.GenerativeModel(model_name="gemini-1.5-pro")
+
+# Prompt the model with text and the previously uploaded image.
+response = model.generate_content([sample_file, "If this picture was a real item, would this product belong in the recycle, garbage, or compost."])
+
+print(response.text)
