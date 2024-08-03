534058739246d08068ee8dbbd7ced725484db5ea
PoloJeans <dylanknguyen04@gmail.com>
2024-08-03 17:53:13 -0400

    no

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import google.generativeai as genai
 genai.configure(api_key=os.environ['API_KEY'])
 
 # Upload the file and print a confirmation.
-sample_file = genai.upload_file(path="jetpack.jpg",
-                            display_name="Jetpack drawing")
+sample_file = genai.upload_file(path="/Users/dylannguyen/Desktop/terrahacksgoogle/dogpiss.png",
+                            display_name="Test")
 
 print(f"Uploaded file '{sample_file.display_name}' as: {sample_file.uri}")
 
@@ -17,7 +17,6 @@ print(f"Retrieved file '{file.display_name}' as: {sample_file.uri}")
 model = genai.GenerativeModel(model_name="gemini-1.5-pro")
 
 # Prompt the model with text and the previously uploaded image.
-response = model.generate_content([sample_file, "If this picture was a real item, would this product belong in the recycle, garbage, or compost."])
+response = model.generate_content([sample_file, "Would this item belong in the recycle, garbage, or compost."])
 
 print(response.text)
-print("wrhfwhifrf")
