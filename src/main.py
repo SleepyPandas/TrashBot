83b62b0a0fab281e70e6db9d4ffd34a7dd57b141
SleepyPandas <149357281+SleepyPandas@users.noreply.github.com>
2025-01-03 13:26:10 -0500

    Added def main for convention + no globals

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
 from CameraHandler import CameraHandler
 
-if __name__ == "__main__":
-    # Webcam part
+
+# wrapped in main to ensure no globals
+def main():
     webcam_handler = CameraHandler()
     webcam_handler.capture_frames()
+
+
+if __name__ == "__main__":
+    main()
